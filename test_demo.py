84d99425239fec6c578b4827d631c4ff73d71295
250399 <kunaldeshmukh2503@gmail.com>
2020-06-04 23:48:38 -0700

    ADDED: used parametrize decorator for testing

--- test_demo.py
+++ test_demo.py
@@ -22,3 +22,20 @@ def test_product():
 @pytest.mark.string
 def test_product_string():
     assert demo_object.product("Hello",3) == "HelloHelloHello" 
+
+
+@pytest.mark.parametrize('element1, element2, result',
+[
+    (3,4,7),
+    ("Hello","World","HelloWorld"),
+    (4.5,5.5,10)
+])
+def test_add_int(element1,element2,result): 
+    assert demo_object.add(element1,element2) == result
+
+@pytest.mark.parametrize('ele1, ele2, ele3',[
+    (3,2,6),
+    ("Hello",2,"HelloHello"),
+])
+def test_prod_string(ele1,ele2,ele3):
+    assert demo_object.product(ele1,ele2) == ele3
