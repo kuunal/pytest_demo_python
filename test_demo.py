5925abbf375dbf4b87414a7720f71a287a2c981a
250399 <kunaldeshmukh2503@gmail.com>
2020-06-04 23:31:37 -0700

    ADDED: used mark flag for testing

--- test_demo.py
+++ test_demo.py
@@ -7,13 +7,18 @@ def setup_module(module):
     global demo_object
     demo_object = demo_operations()
 
-
+@pytest.mark.numbers
 def test_add():
     assert demo_object.add(3,4) == 7
 
+@pytest.mark.string
 def test_add_string():
     assert demo_object.add("Hello ","World") == "Hello World"
 
-
+@pytest.mark.numbers
 def test_product():
     assert demo_object.product(2,5) == 10
+
+@pytest.mark.string
+def test_product_string():
+    assert demo_object.product("Hello",3) == "HelloHelloHello" 
